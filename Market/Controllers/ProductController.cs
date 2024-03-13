4df56e960858c3df4e9f0fd0874470ed720aab56
d_ego <dvegor@gmail.com>
2024-03-13 22:30:15 +0400

    финал

---
+++ Market/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using Market.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("getProducts")]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    var products = context.Products.Select(x => new Product()
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Description = x.Description
+                    });
+                    return Ok(products);
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("postProducts")]
+        public IActionResult PostProducts([FromQuery] string name, string description, int price, int productId)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (!context.Products.Any(x => x.Name.ToLower().Equals(name)))
+                    {
+                        context.Add(new Product()
+                        {
+                            Name = name,
+                            Description = description,
+                            Price = price,
+                            ProductId = productId
+                        });
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+
+
+
+
+    }
+}
